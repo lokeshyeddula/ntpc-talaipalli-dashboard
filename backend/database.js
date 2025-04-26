64341e5fad268bd6beb91ae99941b40b49a38c6c
lokeshyeddula <lokeshyeddula.11@gmail.com>
2025-04-26 11:50:21 +0530

    updated changes

--- backend/database.js
+++ backend/database.js
@@ -1,19 +1,28 @@
+
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('bokbhxclupouxzvunpu1', 'uav3u2vxcgscuzwv', 'uav3u2vxcgscuzwv', {
-    host: 'bokbhxclupouxzvunpu1-mysql.services.clever-cloud.com',
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-    logging: false
-});
+    logging: false,
+  }
+);
+
 
 (async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Database connected successfully.');
-    } catch (error) {
-        console.error('Database connection failed:', error);
-        process.exit(1);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected successfully.');
+  } catch (error) {
+    console.error('Database connection failed:', error.message);
+    process.exit(1);
+  }
 })();
 
 module.exports = sequelize;
