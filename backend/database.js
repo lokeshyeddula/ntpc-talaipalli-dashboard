1f2e5540accd4d25a3e1c5e608e365f05179d98e
lokeshyeddula <lokeshyeddula.11@gmail.com>
2025-04-30 17:04:45 +0530

    updated changes

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,6 @@
- const { Sequelize } = require('sequelize');
+require('dotenv').config({ path: '../.env' });
 
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.MYSQL_DB,
